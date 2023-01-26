f6c9f5691111706a672844b633ca013b4b982a1c
Max Svetlov <5808419+Max-Lumiere@users.noreply.github.com>
2023-01-26 15:35:39 +0200

    acces midifier fix

--- LumiereToolkit/Sources_iOS/UIKitLocalized/UIKitLocalized.swift
+++ LumiereToolkit/Sources_iOS/UIKitLocalized/UIKitLocalized.swift
@@ -1,12 +1,12 @@
 import Foundation
 
-enum UIKitLocalized {
+public enum UIKitLocalized {
     private static let bundle = Bundle(identifier: "com.apple.UIKit")
 
-    static var ok: String { stringForKey("OK") }
-    static var search: String { stringForKey("Search") }
-    static var cancel: String { stringForKey("Cancel") }
-    static var done: String { stringForKey("Done") }
+    public static var ok: String { stringForKey("OK") }
+    public static var search: String { stringForKey("Search") }
+    public static var cancel: String { stringForKey("Cancel") }
+    public static var done: String { stringForKey("Done") }
 
     private static func stringForKey(_ key: String) -> String {
         bundle?.localizedString(forKey: key, value: key, table: nil) ?? key
